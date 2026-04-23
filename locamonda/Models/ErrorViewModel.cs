df4950efa1f0f0e51074158150a1b438986910c2
Amir Elfoly <amerelfoly223@gmail.com>
2026-04-23 16:14:13 +0200

    VS created the main project structure

---
+++ locamonda/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace locamonda.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
