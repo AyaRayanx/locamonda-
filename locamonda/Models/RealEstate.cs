0ae1aa46e8845053f017de4eddca6bee70182d8e
Amir Elfoly <amerelfoly223@gmail.com>
2026-04-24 20:45:52 +0300

    File model/RealEstate

---
+++ locamonda/Models/RealEstate.cs
@@ -0,0 +1,87 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace locamonda.Models
+{
+    public class RealEstate
+    {
+
+        public enum RealEstatetype
+        {
+            Apartment,
+            House,
+            Office,
+            Land,
+            Villa,
+            Shop
+        }
+
+
+
+        [Key]
+        public int Id { get; set; }
+
+
+
+        [Required]
+        [MaxLength(50), MinLength(5)]
+        [Display(Name = "RealEstate Title")]
+        public string Title { get; set; }
+
+
+        [MaxLength(500)]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+
+
+        [Required]
+        [Display(Name = "Price")]
+        public decimal Price { get; set; }
+
+
+
+        [Required]
+        [MaxLength(200)]
+        [Display(Name = "Location")]
+        public string Location { get; set; }
+
+
+
+        [Required]
+        [Display(Name = "RealEstateType")]
+        public RealEstatetype RealEstateType { get; set; }
+
+
+        [Display(Name = "Rooms")]
+        public int Rooms { get; set; }
+
+        [Range(0, 50)]
+        [Display(Name = "Bathrooms")]
+        public int Bathrooms { get; set; }
+
+
+        [Display(Name = "Image")]
+        public string Image { get; set; }
+
+
+        [Display(Name = "DateAdded")]
+        public DateTime DateAdded { get; set; }
+
+        public bool IsApproved { get; set; } = false;
+
+        public bool IsActive { get; set; } = true;
+
+
+
+        /* Navigation Properties */
+
+        /*
+    
+         Favorites
+         Bookings 
+         Orders
+
+        */
+
+    }
+}
--- locamonda/obj/Debug/net8.0/locamonda.AssemblyInfo.cs
+++ locamonda/obj/Debug/net8.0/locamonda.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("locamonda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e16c6b59c4ba615596339e6161a0f1edfbaa77ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdb92d67d738ac565b4f630163fd8724a44f5582")]
 [assembly: System.Reflection.AssemblyProductAttribute("locamonda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("locamonda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
